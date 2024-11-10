fb138ca70d25dd975af0e057a3cd0cfd0fe1fbf8
k0ch3gar <kostik_fetisov@inbox.ru>
2024-11-10 14:39:36 +0300

    refactor: added gitignore

--- cmake-build-debug/lib/glfw/cmake_uninstall.cmake
+++ cmake-build-debug/lib/glfw/cmake_uninstall.cmake
@@ -1,22 +1,22 @@
 
-if (NOT EXISTS "C:/Users/KOSTJA/CLionProjects/megaminx/cmake-build-debug/lib/glfw/install_manifest.txt")
-    message(FATAL_ERROR "Cannot find install manifest: \"C:/Users/KOSTJA/CLionProjects/megaminx/cmake-build-debug/lib/glfw/install_manifest.txt\"")
+if (NOT EXISTS "C:/Users/kosti/Projects/megaminx/cmake-build-debug/lib/glfw/install_manifest.txt")
+    message(FATAL_ERROR "Cannot find install manifest: \"C:/Users/kosti/Projects/megaminx/cmake-build-debug/lib/glfw/install_manifest.txt\"")
 endif()
 
-file(READ "C:/Users/KOSTJA/CLionProjects/megaminx/cmake-build-debug/lib/glfw/install_manifest.txt" files)
+file(READ "C:/Users/kosti/Projects/megaminx/cmake-build-debug/lib/glfw/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 
 foreach (file ${files})
   message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
   if (EXISTS "$ENV{DESTDIR}${file}")
-    exec_program("C:/Users/KOSTJA/AppData/Local/Programs/CLion/bin/cmake/win/x64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+    exec_program("C:/Users/kosti/AppData/Local/Programs/CLion/bin/cmake/win/x64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
                  OUTPUT_VARIABLE rm_out
                  RETURN_VALUE rm_retval)
     if (NOT "${rm_retval}" STREQUAL 0)
       MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
     endif()
   elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
-    EXEC_PROGRAM("C:/Users/KOSTJA/AppData/Local/Programs/CLion/bin/cmake/win/x64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+    EXEC_PROGRAM("C:/Users/kosti/AppData/Local/Programs/CLion/bin/cmake/win/x64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
                  OUTPUT_VARIABLE rm_out
                  RETURN_VALUE rm_retval)
     if (NOT "${rm_retval}" STREQUAL 0)
